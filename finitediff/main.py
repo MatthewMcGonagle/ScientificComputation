e699d73f03f1d1bc7beb9a41a1eed45e4aa22771
Matthew McGonagle <mwmcgonagle@gmail.com>
2017-02-03 01:21:27 -0800

    Working on doing the reduction using one array for each interior point of mesh. Comments record how to reduce upwards in first pass.

--- finitediff/main.py
+++ finitediff/main.py
@@ -23,39 +23,75 @@ def g(x,y):
 
 print( "Initializing Data Arrays" )
 
-varcoeff = np.zeros( (nvarpoints[0], nvarpoints[1], 3, nvarpoints[1]) )
+varcoeff = np.zeros( ( nvarpoints[0]
+		     , nvarpoints[1]
+		     , nvarpoints[1]
+		     ) )
+scaling = -1.0 / (dvar[0]**-2 + dvar[1]**-2) / 2.0
+diffcoeffsize = np.array( [dvar[0]**-2, dvar[1]**-2] ) * scaling
 for i in range(nvarpoints[0]):
         for j in range(nvarpoints[1]):
-                if i > 0:
-                        varcoeff[i][j][0][j] = dvar[0]**-2
-                if i < nvarpoints[0] - 1 :
-                        varcoeff[i][j][2][j] = dvar[0]**-2
                 if j > 0:
-                        varcoeff[i][j][1][j-1] = dvar[1]**-2
+                        varcoeff[i][j][j-1] = diffcoeffsize[1] 
                 if j < nvarpoints[1] - 1 :
-                        varcoeff[i][j][1][j+1] = dvar[1]**-2
-                varcoeff[i][j][1][j] = dvar[0]**-2 + dvar[1]**-2
-                varcoeff[i][j][1][j] *= -2.0
+                        varcoeff[i][j][j+1] = diffcoeffsize[1] 
+                varcoeff[i][j][j] = 1.0 
 
-topbottom = np.fromfunction( lambda i, j : f(varpoints[0][j], varbounds[1][i])
+topbottom = np.fromfunction( lambda i, j : f(varpoints[0][j], varbounds[1][i]) 
                                , (2, nvarpoints[0])
                                , dtype = int
                                )
 
-leftright = np.fromfunction( lambda i, j: f(varbounds[0][i], varpoints[1][j])
+
+leftright = np.fromfunction( lambda i, j: f(varbounds[0][i], varpoints[1][j]) 
                                  , (2, nvarpoints[1])
                                  , dtype = int
                                  )
 
-rhsterms = np.fromfunction( lambda i, j : g(varpoints[0][i], varpoints[1][j])
+rhsterms = np.fromfunction( lambda i, j : g(varpoints[0][i], varpoints[1][j]) * scaling
                          , (nvarpoints[0], nvarpoints[1]) 
                          , dtype = int
                          )
 
 for i in range(nvarpoints[0]) :
-        rhsterms[i][0] -= topbottom[0][i] * dvar[1]**-2
-        rhsterms[i][-1] -= topbottom[1][i] * dvar[1]**-2
+        rhsterms[i][0] -= topbottom[0][i] 
+        rhsterms[i][-1] -= topbottom[1][i]
 
 for i in range(nvarpoints[1]):
-        rhsterms[0][i] -= leftright[0][i] * dvar[0]**-2
-        rhsterms[-1][i] -= leftright[1][i] * dvar[0]**-2
+        rhsterms[0][i] -= leftright[0][i]
+        rhsterms[-1][i] -= leftright[1][i]
+
+# Reduce
+# After completion, varpoints[xi][j][k] = coefficient for ( xi + 1, k ) when k <= j
+#                                       = coefficient for ( xi, k ) when k > j
+def reduceupycoord_notlastx(xi):
+	for yj in range(nvarpoints[1]):
+
+		# varpoints[xi][j][k] holds information on the coefficients in the equation for the
+		# points (xi, j). Some coefficients are held implicitly and some coefficients are held
+		# explicitly in varpoints[xi][j][[:].
+		#
+		# At the beginning of loop on yj:
+		#
+		# Should have varpoints[xi][j][k] = coeff of ( xi + 1 , k) for all j and all k < yj,
+		#                                 = coeff of (xi, k) for all yj <= k.
+		#
+		# Also, implictly the coefficient for (xi + 1, yj) is diffcoeffsize[0].
+		#
+		# Finally, implicitly the equation for any point (xi , k) with k < yj 
+		# has the coefficient 1.0 for itself, (xi, k).
+
+		scaling = 1.0 / varpoints[xi][yj][yj]
+
+		for yk in range(yj):
+			varpoints[xi][yj][yk] *= scaling
+
+		fixedcoeff = diffcoeffsize[0] * scaling 
+		varpoints[xi][yj][yj] = fixedcoeff
+
+		for yk in range(yj+1, nvarpoints[1]):
+			varpoints[xi][yj][yk] *= scaling
+
+		for yk in range(yj + 1, nvarpoints[1]):
+			varpoints[xi][yk][yj] = -* fixedcoeff
+
